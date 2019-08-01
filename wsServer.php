c752c0a6f4df824d5b6cff73d397fc4163e87a36
HPPang <hhtjim@yeah.net>
2019-08-01 21:55:30 +0800

    debug

--- wsServer.php
+++ wsServer.php
@@ -66,10 +66,10 @@ function user_handshake(swoole_http_request $request, swoole_http_response $resp
         global $stack;
         $now = time();
         foreach ($stack as $k => $v){
-//            if( $now - $v['time'] > 5 * 60 ){//消息时间 大于5分钟
+            if( $now - $v['time'] > 5 * 60 ){//消息时间 大于5分钟
 //                unset($stack[$k]);
 //                continue;
-//            }
+            }
 //            $server->push($fd, $v['json']);
         }
 
