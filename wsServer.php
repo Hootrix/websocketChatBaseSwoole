b6e4718fcb7857f5a0ca8a37b58ba23c7469709e
HPPang <hhtjim@yeah.net>
2019-08-01 20:01:55 +0800

    debug

--- wsServer.php
+++ wsServer.php
@@ -124,7 +124,7 @@ $server->on('message', function (swoole_websocket_server $_server, $frame) {
 $server->on('close', function ($_server, $fd) {
     echo "client {$fd} closed\n";
 
-    $json = json_encode(['clients' => count($_server->connections),'action'=>'close']);
+    $json = json_encode(['clients' => count($_server->connections) - 1,'action'=>'close']);
     foreach ($_server->connections as $item) {
         $_server->push($item, $json);
     }
